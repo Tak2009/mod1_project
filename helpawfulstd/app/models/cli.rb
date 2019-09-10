d0eca482cf238672a41351dfd6faed5b369d893e
github username <github email address>
2019-09-10 11:36:55 +0100

    created CLI calss

--- helpawfulstd/app/models/cli.rb
+++ helpawfulstd/app/models/cli.rb
@@ -1,49 +1,78 @@
-# require_relative '../../config/environment'
-# require 'tty-prompt'
+require_relative '../../config/environment'
+require 'tty-prompt'
 
-#     def run
-#         puts "Hello. Please log in or sign up"
-#         prompt = TTY::Prompt.new
-#         choice = prompt.select("Please, select ", ["Sign up as Student","Sign up as Tutor", "Login", "Exit"])
-#         if choice == "Sign up as Student" 
-#              student_user 
-#         elsif choice == "Sign up as Tutor"
-#              tutor_user
-#         elsif choice == "Login"
-#              login 
-#         else 
-#              puts "We hope we will see you soon again!" 
-#              sleep(1)
-#         end 
-#     end 
+# do NOT use any  variable names which are used as attributes otherwise you will screw up!!!!! >>>> Undefined Class Method!!!
+# do NOT use any  variable names which are used as attributes otherwise you will screw up!!!!! >>>> Undefined Class Method!!!
+# do NOT use any  variable names which are used as attributes otherwise you will screw up!!!!! >>>> Undefined Class Method!!!
+# do NOT use any  variable names which are used as attributes otherwise you will screw up!!!!! >>>> Undefined Class Method!!!
 
-#     def login
-#         prompt = TTY::Prompt.new
-#         contact_email = prompt.ask("What is your email address??")
-#         password = prompt.ask("What is your password??") # I will skip this process
-#         if contact_email == Tutor.all.find_by(contact_email: contact_email) 
-#              tutor_user_screen
-#         elsif contact_email == Student.all.find_by(contact_email: contact_email)
-#              student_user_screen
-#         else
-#              "I can not find your log-in id. Please check"
+require 'pry'
+
+class CLI
+
+    def initialize
+        @student_u = nil
+        @tutor_u = nil
+    end
+
+    def run
+        puts "Hello. Please log in or sign up"
+        prompt = TTY::Prompt.new
+        choice = prompt.select("Please, select ", ["Sign up as Student", "Sign up as Tutor", "Login", "Exit"])
+        if choice == "Sign up as Student" 
+             student_signup
+        elsif choice == "Sign up as Tutor"
+             tutor_user
+        elsif choice == "Login"
+             login 
+        else 
+             puts "We hope we will see you soon again!" 
+             sleep(10)
+        end 
+    end 
+
+    def student_signup
+        puts "blablabla"
+    end
+
+    def login
+        prompt = TTY::Prompt.new
+        type = prompt.select("Please, select ", ["Login as Tutor?", "Login as Student?"])
+        if type == "Log in as Tutor?"
+            login_tutor
+        else 
+            login_student
+        end
+    end
+
+    def login_student
+        prompt = TTY::Prompt.new
+        email = prompt.ask("What is your email address??")
+        pw = prompt.ask("What is your password??") 
+        @student_u = Student.all.find_by(contact_email: email)
+
         
-#         # Log in successful
-#         puts "Thank you!"
-#         sleep(10)
-#     end
+        if @student_u == nil # Student thinks he/she has the account but actually not case
+            puts "We can not find your email. Please sign up" 
+            student_signup
+        elsif email == @student_u.contact_email && pw == @student_u.password # Login process. both ID and password must match agfainst database
+            student_profile_screen
+        else
+            puts "Either your email or password is incorrect. Please try again"
+        end
+        
+    end
 
-#     def student_user
-#     prompt = TTY::Prompt.new
-#     s_profile_name = prompt.ask("What is your profile name?")
-#     location = prompt.ask("in which city?")
-#     age = prompt.ask("your age?")
-#     contact_email = prompt.ask("your contact email?")
-#     user = Student.create(s_profile_name: s_profile_name, location: location, age: age, contact_email: contact_email)
-#     puts "Thank you. All done now"
-#     #    $state["user"] = user 
-#     #         questions_interface
-#     end
+    def student_profile_screen
+        prompt = TTY::Prompt.new
+        puts "Welcome back, #{@student_u.s_profile_name}!"
+    end
+        
+    
+
+    
+
+end
 
 
 
--- helpawfulstd/bin/run.rb
+++ helpawfulstd/bin/run.rb
@@ -1,3 +1,25 @@
 require_relative '../config/environment'
 
-run
+cli = CLI.new 
+cli.run
+
+
+# I used to have the setting below.
+
+# ====================================================
+# require_relative '../config/environment'
+# run 
+# ====================================================
+
+# With this setting, my cli file could not pass any variable to other methods.
+# cli.rb was not a class file.
+# Therefore, to resolve my issue, 1. set a class and 2. write instance methods wioth in the class and hence those instance variables were set so that those varibales can be passed
+
+# class CLI
+
+#     def initialize
+#         @student_u = nil
+#         @tutor_u = nil
+#     end
+# end
+
