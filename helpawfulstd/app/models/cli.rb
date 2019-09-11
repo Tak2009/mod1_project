162503476a960f7c8c789f42e14919a113a1bda7
github username <github email address>
2019-09-11 17:24:27 +0100

    logic chnaged in Rearch

--- helpawfulstd/app/models/cli.rb
+++ helpawfulstd/app/models/cli.rb
@@ -32,6 +32,7 @@ class CLI
 # ======================= From here, For Student User Type=====================================================================================
 
     # Student C
+# No validation for email yet
     def student_signup
         puts "Please provide the following information to create your profile"
         prompt = TTY::Prompt.new
@@ -50,7 +51,7 @@ class CLI
     def login
         prompt = TTY::Prompt.new
         type = prompt.select("Please, select ", ["Login as Tutor?", "Login as Student?"])
-        if type == "Log in as Tutor?"
+        if type == "Login as Tutor?"
             login_tutor
         else 
             login_student
@@ -60,7 +61,6 @@ class CLI
     # For both Student and Tutor users
     def logout 
         puts "Goodby! See you soon!"
-
     end
     
     # Student Login
@@ -109,19 +109,29 @@ class CLI
 
         prompt = TTY::Prompt.new
         menu = prompt.select("Which serach would you fancy?", ["Simple(search by location and language you wanna learn)", "Advanced(based on rerviews)"])
-        if menu == "Advanced"
+        if menu == "Advanced(based on rerviews)"
             s_advanced_search_tutor
         else
             place = prompt.ask("Enter your location or anywhere you wanna check!")
             which_language = prompt.ask("Which language you wanna learn?")
-            puts "here you go!"
-            Tutor.all.where(location: place).where(language: which_language).each do |element_hash| p element_hash end
-            if prompt.yes?("Wanna serch again? Otherwise going back to your profile screen!")
-                s_search_tutor
-            else
-                student_profile_screen
-            end
+         
+            search_result_t = Tutor.all.where(location: place).where(language: which_language)
+                if search_result_t.length == 0
+                    puts "No match... Try again :("
+                    
+                else
+                    puts "here you go!"
+                    search_result_t.map{|t| {:name => t.t_profile_name, :locartion => t.location, :language => t.language, :experience => t.experience, :hourly_rate => t.price, :email => t.contact_email}}
+                    # search_result_t.each do |element_hash| p element_hash end  ==> this returns unnecessary information
+                end  
         end
+
+        if prompt.yes?("Wanna serch again? Otherwise going back to your profile screen!")
+            s_search_tutor
+        else
+           puts "redirecting...."
+        end
+        student_profile_screen
     end
 
 
@@ -137,7 +147,7 @@ class CLI
         puts "we need more information!"
         place = prompt.ask("In which area, are you looking?") # 3 from Tutor table
         experieced_or_not = prompt.ask("Select minimum experience level with 5 being highest plase.") # 4 from Tutor table
-        "Ok, thank you! hold on a sec pllease, We are serching for you!"
+        puts "Ok, thank you! hold on a sec pllease, We are serching for you!"
         # narrow down with #1 and #2
         reviews_array = Review.all.where(language: what_language).where("rating_for_tutor >= #{tutor_rating}")
         tutor_id_only_array = reviews_array.map{|tutor_with_conditions| tutor_with_conditions.tutor_id}.uniq # reviews contains multiple reviews for 1 specific tutor user
@@ -156,8 +166,9 @@ class CLI
         if prompt.yes?("Wanna serch again? Otherwise going back to your profile screen!")
             s_search_tutor
         else
-            student_profile_screen
+            puts "redirecting...."
         end
+        student_profile_screen
     end
 
     # Student W
@@ -165,14 +176,14 @@ class CLI
         puts "Please answer follwing questions. You can write reviews for tutors in our platform. Make sure you treat people fairly and as nice as pissbile :)"
         prompt = TTY::Prompt.new
         level = prompt.ask("What is your knowledge level with 5 being highest?")
+    # Need to be cahnged to a list with email
         email = prompt.ask("What is your tutor email? We need this for validation!") # filterling by name is too weak as there must be a lot of daves, toms for example. email is considered as unique enough
-        what_language = prompt.ask("In what language?")
         tutor_rating = prompt.ask("Give a rating please with 5 being highest")
         something_to_say = prompt.ask("Any comment?")
 
         t_id_by_email = Tutor.all.find_by(contact_email: email) # get the referece for the tutor
 
-        Review.create(student_id: @student_u.id, student_own_level: level, tutor_id: t_id_by_email.id, language: what_language, rating_for_tutor: tutor_rating, comment: something_to_say)
+        Review.create(student_id: @student_u.id, student_own_level: level, tutor_id: t_id_by_email.id, language: t_id_by_email.language, rating_for_tutor: tutor_rating, comment: something_to_say)
         puts "Thank youfor your review. We always appreciate your feedback!!"
         
         student_profile_screen
@@ -182,18 +193,13 @@ class CLI
     # Student U
     def s_update_profile
         prompt = TTY::Prompt.new
-        attr = prompt.select("What information would you lkike to update?", Student.column_names)
-    # binding.pry
-        if attr == "id" || attr == "created_at" || attr == "updated_at"
-            puts "Oops, you can not change it, please select something else"
-            s_update_profile
-        else 
-           new_info = prompt.ask("Please enter the new #{attr}")
-           @student_u.update({attr => new_info})
-        end
-            
+        selection_excluding_system_generated = Student.column_names.select{|attr| attr != "id" && attr != "created_at" && attr != "updated_at"} 
+        attr = prompt.select("What information would you lkike to update?", selection_excluding_system_generated)
+        new_info = prompt.ask("Please enter the new #{attr}")
+        @student_u.update({attr => new_info})
+        
         if prompt.yes?('Would you like to update more information?')
-           s_update_profile
+            s_update_profile
         else
             student_profile_screen
         end
@@ -213,33 +219,55 @@ class CLI
            student_profile_screen
         end
     end
-   
-     # ======================= From here, For Tutor User Type=====================================================================================
+    
+# ======================= From here, For Tutor User Type=====================================================================================
      
      # Totor C 
+# no validation applied on email. need to woerk on iuf time allows
      def tutor_signup
-        puts "Please provide the following information to create your profile"
+        puts "Please provide the following information to create your profile, tutor"
         prompt = TTY::Prompt.new
-        name = prompt.ask("Your profile name?")
-        place = prompt.ask("Your city?")
-        language_your_speciality = prompt.ask("Which language you can teach?")
-        email = prompt.ask("Your email? This will be used as your login ID once your profile created")
-        pw = prompt.ask("Your password?")
-        @tutor_u = Tutor.create(s_profile_name: name, location: place, age: how_old, wanna_learn: which_language, contact_email: email, password: pw)
-        puts "Thank you! All done!"
+        name = prompt.ask("Your profile name, tutor?")
+        place = prompt.ask("Your city, tutor?")
+        language_your_speciality = prompt.ask("Which language you can teach, tutor?")
+        how_good = prompt.ask("Your experience level with 5 being highest, tutor?")
+        hourly_rate = prompt.ask("How expensive are you (hourly rate), tutor?")
+        email = prompt.ask("Your email, tutor? This will be used as your login ID once your profile created")
+        pw = prompt.ask("Your password, tutor?")
+        @tutor_u = Tutor.create(t_profile_name: name, location: place, language: language_your_speciality, experience: how_good, price: hourly_rate, contact_email: email, password: pw)
+        puts "Thank you, tutor! All done!"
         tutor_profile_screen
      end
+     
+     # Tutor Login
+     def login_tutor
+        prompt = TTY::Prompt.new
+        email = prompt.ask("What is your email address, tutor??")
+        pw = prompt.ask("What is your password, tutor??") 
+        @tutor_u = Tutor.all.find_by(contact_email: email)
+
+        if @tutor_u == nil # Tutor thinks he/she has the account but actually not case
+            puts "We can not find your email, tutor. Please sign up" 
+            tutor_signup
+        elsif email == @tutor_u.contact_email && pw == @tutor_u.password # Login process. both ID and password must match agfainst database
+            tutor_profile_screen
+        else
+            puts "Either your email or password is incorrect. Please try again, tutor" # email exits but app does not want to explicitly notify that the user has an account at this stage
+            login_tutor
+        end
+        
+    end
 
      # Tutor Main screen
     def tutor_profile_screen
         prompt = TTY::Prompt.new
-        puts "Welcome back, #{@tutor_u.s_profile_name}!"
-        menu = prompt.select("Please, select one of your options!", ["Update Profile", "Search Tutor", "Delete Profile","Log out"])
+        puts "Welcome back, #{@tutor_u.t_profile_name}, tutor!"
+        menu = prompt.select("Select one of your options, tutor!", ["Update Profile", "Search Student", "Delete Profile", "Log out"])
         
         if menu == "Update Profile"
             t_update_profile
-        elsif menu == "Search Tutor"                  
-            t_search_tutor
+        elsif menu == "Search Student"                  
+            t_search_student
         elsif menu == "Delete Profile"
             t_delete_profile
         else
@@ -248,15 +276,72 @@ class CLI
     end
 
 
+    # Tutor R very simple looking into Student table. This search is NOT Review dependant
+# data input validation NOT implemented at all. Need  to work on if time allows
+    def t_search_student
+
+        prompt = TTY::Prompt.new
+        menu = prompt.select("Which serach would you fancy, tutor?", ["Simple(search by location)", "Advanced(based on rerviews)"])  # no need to input langiage in Tutor user as it has langage attr assigned 
+        if menu == "Advanced(based on rerviews)"
+            t_advanced_search_student
+        else
+            place = prompt.ask("Enter your location or anywhere you wanna check, tutor!")
+            language_to_teach = prompt.ask("Which language?")
+            serach_result = Student.all.where(location: place).where(wanna_learn: language_to_teach)
+    # binding.pry
+               if serach_result.length == 0
+                  puts "No match, tutor... :("
+                  t_search_student
+               else
+                  puts "here you go!"
+                  serach_result.each do |element_hash| p element_hash end  # need t think of better user profile structure for tutor
+               end
+        end
+
+        if prompt.yes?("Wanna serch again? Otherwise going back to your profile screen!")
+            t_search_student
+        else
+            tutor_profile_screen
+        end
+        
+    end
+
+    def t_advanced_search_student
+
+        puts "Let me help you search a good tutor near you!"
+        prompt = TTY::Prompt.new
+        tutor_rating = prompt.ask("Tutor, please select minimum tutor rating from student? Am sure you wanna find an easygoing student!") # 1 from Review table
+        difficult_to_teach = prompt.ask("Lower is more beginner. With 5(advanced student) being highest. If you wanna avoid beginners, enter a higher number") # 2 from Review table
+        puts "We need more information, tutor!"
+        place = prompt.ask("In which area, are you looking, tutor?") # 3 Student table
+        
+        puts "Ok, tutor! Hold on a sec pllease, We are serching for you!"
+        # narrow down with #1 and #2 and language(self)
+        reviews_array = Review.all.where(language: @tutor_u.language).where("student_own_level >= #{difficult_to_teach}").where("rating_for_tutor >= #{tutor_rating}")
+        student_id_only_array = reviews_array.map{|student_with_conditions| student_with_conditions.student_id}.uniq # students can have many reviews for 1 specific tutor user
+        # need to sort with #3 in Student table
+        student_list_with_conditions = Student.all.where(id: student_id_only_array).where(location: place)
+     
+        if student_list_with_conditions.length == 0
+            puts "No match :("
+
+        else
+            puts "Here is the result!"
+            student_list_with_conditions.each do |element_hash| p element_hash end
+            
+        end
+
+        if prompt.yes?("Wanna serch again? Otherwise going back to your profile screen!")
+            t_search_tutor
+        else
+            tutor_profile_screen
+        end
+    end
+
+        
         # def go_to_exit
         #     puts "Byebye"
         # end
-    
-
-
-
-    
-
 end
 
 
